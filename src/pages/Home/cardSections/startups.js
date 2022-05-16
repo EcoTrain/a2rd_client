12ae7d97b093c1ac8414799540722efb9dc1abb5
coprocoder <mog.ie@mail.ru>
2022-05-17 01:53:26 +0700

    Fix startups styles & ref

--- src/pages/Home/cardSections/startups.js
+++ src/pages/Home/cardSections/startups.js
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-import QueueAnim from "rc-queue-anim";
 import ScrollAnim from "rc-scroll-anim";
 import { Layout } from "antd";
 import "antd/dist/antd.min.css";
@@ -29,14 +28,9 @@ const HomeStartups = () => {
         <div className="font-title-h1 text-center" style={{ marginTop: "2em" }}>
           Our startups
         </div>
-        <QueueAnim
-          type={["bottom", "top"]}
-          delay={200}
-          duration={1000}
-          className="gridCardsView lineGridCardsView"
-        >
+        <div className="gridCardsView lineGridCardsView">
           {startupsCardsInfo.map((x, i) => renderCard(x, i))}
-        </QueueAnim>
+        </div>
       </ScrollOverPack>
     </Layout>
   );
@@ -47,15 +41,19 @@ const renderCard = (info, i) => {
   const [hover, setHover] = useState(false);
 
   const onHover = () => {
-    cardRef.current.style.background = info.bg;
-    cardRef.current.style.minWidth = "300px";
-    cardRef.current.style.transition = "min-width 1.5s linear;";
+    if (cardRef.current) {
+      cardRef.current.style.background = info.bg;
+      cardRef.current.style.minWidth = "300px";
+      cardRef.current.style.transition = "min-width 1.5s linear";
+    }
     setHover(true);
   };
 
   const onUnHover = () => {
-    cardRef.current.style.background = null;
-    cardRef.current.style.minWidth = "unset";
+    if (cardRef.current) {
+      cardRef.current.style.background = null;
+      cardRef.current.style.minWidth = "unset";
+    }
     setHover(false);
   };
 
