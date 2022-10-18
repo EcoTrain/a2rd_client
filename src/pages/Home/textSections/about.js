9e060dd7439cc10deae6f14ec90a2eaf824116bb
coprocoder <mog.ie@mail.ru>
2022-10-18 20:24:58 +0600

    fix: change about text

--- src/pages/Home/textSections/about.js
+++ src/pages/Home/textSections/about.js
@@ -1,14 +1,14 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import QueueAnim from "rc-queue-anim";
 import ScrollAnim from "rc-scroll-anim";
-import { scroller } from "react-scroll";
-import { useTranslation } from "react-i18next";
-import { Layout } from "antd";
+import {scroller} from "react-scroll";
+import {useTranslation} from "react-i18next";
+import {Layout} from "antd";
 import "antd/dist/antd.min.css";
 
 const ScrollOverPack = ScrollAnim.OverPack;
 
-const AboutDescription = ({ t }) => {
+const AboutDescription = ({t}) => {
   return (
     <div key="homeAboutDescription" id="homeAboutDescription">
       <div className="section-title font-title-h1 text-center">
@@ -19,14 +19,14 @@ const AboutDescription = ({ t }) => {
   );
 };
 
-const AboutFounder = ({ t }) => {
+const AboutFounder = ({t}) => {
   const imgAlexey =
     process.env.PUBLIC_URL + "/static/images/index/aleksey.webp";
   return (
     <div
       key="homeAboutFounder"
       id="homeAboutFounder"
-      style={{ margin: "1em 0", cursor: "pointer" }}
+      style={{margin: "1em 0", cursor: "pointer"}}
       onClick={() => {
         scroller.scrollTo("contacts", {
           duration: 0,
@@ -36,10 +36,10 @@ const AboutFounder = ({ t }) => {
       <img
         className="avatar"
         src={imgAlexey}
-        style={{ height: 100, width: 100, marginBottom: "1em" }}
+        style={{height: 100, width: 100, marginBottom: "1em"}}
       />
       <div>
-        <div>{t("about.signature")}</div>
+        <div className="text">{t("about.signature")}</div>
         <div>{t("about.name")}</div>
         <div>{t("about.position")}</div>
       </div>
@@ -49,7 +49,7 @@ const AboutFounder = ({ t }) => {
 
 const HomeAbout = () => {
   const pageRef = useRef();
-  const { t } = useTranslation(["home"]);
+  const {t} = useTranslation(["home"]);
   return (
     <Layout
       id="homeAbout"
@@ -62,10 +62,10 @@ const HomeAbout = () => {
         playScale={0}
         className="section-content text-center"
       >
-        <QueueAnim type={["bottom", "top"]} className="section-content-block">
-          {AboutDescription({ t })}
-          {AboutFounder({ t })}
-        </QueueAnim>
+        <div className="section-content-block">
+          {AboutDescription({t})}
+          <QueueAnim type={["bottom", "top"]}>{AboutFounder({t})}</QueueAnim>
+        </div>
       </ScrollOverPack>
     </Layout>
   );
