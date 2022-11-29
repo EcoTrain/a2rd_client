0aeabcdc047857ef6d149a3b8fa3e0f6d7f0baa6
coprocoder <mog.ie@mail.ru>
2022-11-29 17:04:32 +0600

    fix: hide x-framework

--- src/pages/Home/cardSections/cardsConfig.js
+++ src/pages/Home/cardSections/cardsConfig.js
@@ -97,14 +97,14 @@ export const startupsCardsInfo = [
       },
     },
   },
-  {
-    title: "item.framework.title",
-    previewText: "item.framework.previewText",
-    shortText: "item.framework.shortText",
-    text: "item.framework.text",
-    bg: "var(--darkWhite)",
-    id: "startup_xwm",
-  },
+  // {
+  //   title: "item.framework.title",
+  //   previewText: "item.framework.previewText",
+  //   shortText: "item.framework.shortText",
+  //   text: "item.framework.text",
+  //   bg: "var(--darkWhite)",
+  //   id: "startup_xwm",
+  // },
   {
     title: "item.airflows.title",
     previewText: "item.airflows.previewText",
